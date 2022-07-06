9560f4719901505a19bae7a38186d18d36123ac7
Coral-Ann <coral.baker@outlook.com>
2022-07-06 12:29:22 +0100

    Tested/added return true if walk count is 10 and returns to position

--- lib/ten_minute_walk.rb
+++ lib/ten_minute_walk.rb
@@ -1,5 +1,5 @@
 def ten_minute_walk?(walk)
-  if walk.count == 10
+  if walk.length == 10 && walk.count('n') == walk.count('s') && walk.count('w') == walk.count('e')
     true
   else
     false
--- spec/ten_minute_walk_spec.rb
+++ spec/ten_minute_walk_spec.rb
@@ -4,4 +4,8 @@ describe '#ten_minute_walk?' do
   it 'should return false if walk count is not 10' do
     expect(ten_minute_walk?(['e', 's'])).to eq false
   end
+
+  it 'should return true if walk count is 10 and user returns to original position' do
+    expect(ten_minute_walk?(['w', 's', 'e', 'e', 'n', 'n', 'e', 's', 'w', 'w'])).to eq true
+  end
 end
